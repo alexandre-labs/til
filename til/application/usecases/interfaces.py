e72a1a28381f336b59f7371783e8014bf83f6b07
Alexandre <git@alexndr.dev>
2020-01-10 16:40:07 -0300

    add usecases interaces
    The use case request and response could be just an implicit convention,
    but I think this is a simple way to document the interface

---
+++ til/application/usecases/interfaces.py
@@ -0,0 +1,31 @@
+import abc
+
+
+# TODO: Add type hints
+#
+#
+class UseCaseRequest(abc.ABC):
+
+    @abc.abstractclassmethod
+    def create(cls, **kwargs):
+        return NotImplemented
+
+
+class UseCaseResponse(abc.ABC):
+
+    def __init__(self, value):
+        self._value = value
+
+    @abc.abstractproperty
+    def result(self):
+        return NotImplemented
+
+
+class UseCase(abc.ABC):
+
+    def __init__(self, repository=None):
+        self.repository = repository
+
+    @abc.abstractmethod
+    def execute(self, request):
+        return NotImplemented
