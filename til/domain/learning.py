f873bc2295e8124f692c4983269a5952b7dcb772
Alexandre <git@alexndr.dev>
2020-01-10 13:09:19 -0300

    add domain model Learning

---
+++ til/domain/learning.py
@@ -0,0 +1,10 @@
+import datetime
+from dataclasses import dataclass
+
+
+@dataclass
+class Learning:
+    """The main model in the TIL"""
+    title: str
+    description: str
+    timestamp: datetime.datetime
