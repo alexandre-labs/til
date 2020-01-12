f41b7dcc118723357a8aaa6477e83cf7ccf4e91c
Alexandre <git@alexndr.dev>
2020-01-12 18:32:49 -0300

    add initial manage script
    * Add cli command to init a local repository
    * Add gitpython as dep.

--- til/ports/cli.py
+++ til/ports/cli.py
@@ -1,7 +1,10 @@
 import datetime
+import pathlib
 import sys
 
 import click
+import git
+from dynaconf import settings
 
 
 @click.command()
@@ -24,5 +27,27 @@ def til(title):
     click.echo(f"{banner} - {datetime.date.today()}")
 
 
-if __name__ == "__main__":
-    til()
+@click.command('repo-init')
+@click.argument('repo-name')
+def repo_init(repo_name):
+
+    # TODO: implement a better map between name and repo
+    if repo_name == 'local':
+
+        til_data_path = pathlib.Path(settings.REPOSITORIES.LOCAL.DATA_PATH)
+
+        if not til_data_path.is_dir():
+            # Read and write for the owner; only read for everybody else
+            til_data_path.mkdir(parents=True)
+
+        git.Repo.init(til_data_path)
+
+        click.echo("Local repository initialized successfully")
+
+
+@click.group()
+def manage():
+    pass
+
+
+manage.add_command(repo_init)
