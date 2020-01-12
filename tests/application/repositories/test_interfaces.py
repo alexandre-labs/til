4649eb6bbbee8f475de522a17071a95202a91559
Alexandre <git@alexndr.dev>
2020-01-12 18:36:55 -0300

    test: remove outdated test

--- tests/application/repositories/test_interfaces.py
+++ tests/application/repositories/test_interfaces.py
@@ -3,21 +3,10 @@ import pytest
 from til.application.repositories import interfaces
 
 
-def test_learning_repository_mandatory_ping():
-
-    class FakeLearning(interfaces.LearningRepository):
-        def save(self, learning):
-            pass
-
-    with pytest.raises(TypeError):
-        FakeLearning()
-
-
 def test_learning_repository_mandatory_save():
 
     class FakeLearning(interfaces.LearningRepository):
-        def ping(self):
-            pass
+        pass
 
     with pytest.raises(TypeError):
         FakeLearning()
