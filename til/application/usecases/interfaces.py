38be4227a7ca74526b9c21812ff051fefe5967e4
Alexandre <git@alexndr.dev>
2020-01-13 19:07:25 -0300

    refactoring: replace use case interfaces/base classes
    * Interface to represent valid and invalid request
    * Base class to communicate success/error response

--- til/application/usecases/interfaces.py
+++ til/application/usecases/interfaces.py
@@ -4,21 +4,52 @@ import abc
 # TODO: Add type hints
 #
 #
-class UseCaseRequest(abc.ABC):
+class InvalidRequest:
+
+    def __init__(self):
+        self.errors = []
+
+    def __bool__(self):
+        return False
+
+    def add_error(self, item, message):
+        self.errors.append({item: message})
+
+
+class ValidRequest(abc.ABC):
+
+    def __bool__(self):
+        return True
 
     @abc.abstractclassmethod
-    def create(cls, **kwargs):
+    def create(self, **kwargs):
         return NotImplemented
 
 
-class UseCaseResponse(abc.ABC):
+class Response(abc.ABC):
 
-    def __init__(self, value):
-        self._value = value
+    def __init__(self, result):
+        self.result = result
 
-    @abc.abstractproperty
-    def result(self):
+    @abc.abstractmethod
+    def __bool__(self):
         return NotImplemented
+
+
+class Error(Response):
+
+    def __bool__(self):
+        return False
+
+    @classmethod
+    def build_from_invalid_request(cls, invalid_request):
+        return Error(result=invalid_request.errors)
+
+
+class Success(Response):
+
+    def __bool__(self):
+        return True
 
 
 class UseCase(abc.ABC):
